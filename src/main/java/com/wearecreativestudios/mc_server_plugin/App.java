d2db98161064b9166705ccbb2fe9815f63f9faa8
Zeb McCorkle <zmccorkle@wearecreativestudios.com>
2014-01-02 10:26:12 -0600

    First Commit w/ Code

---
+++ src/main/java/com/wearecreativestudios/mc_server_plugin/App.java
@@ -0,0 +1,13 @@
+package com.wearecreativestudios.mc_server_plugin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
